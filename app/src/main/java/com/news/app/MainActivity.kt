613b813a7bfcbe52c392982a01a6caf08aff0463
Aashiq <ashik.cse.developer@gmail.com>
2024-08-20 15:44:56 +0600

    loading indicator

--- app/src/main/java/com/news/app/MainActivity.kt
+++ app/src/main/java/com/news/app/MainActivity.kt
@@ -2,9 +2,12 @@ package com.news.app
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.transition.Visibility
+import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.webkit.WebView
 import android.widget.EditText
+import android.widget.ProgressBar
 import android.widget.TextView.OnEditorActionListener
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -13,6 +16,7 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     private lateinit var webView: WebView
     private lateinit var editText: EditText
+    private lateinit var loading: ProgressBar
 
 
     @SuppressLint("SetJavaScriptEnabled")
@@ -21,14 +25,19 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+
         webView = findViewById(R.id.webView)
         editText = findViewById(R.id.editText)
+        loading = findViewById(R.id.loading)
 
         // this will enable the javascript.
         webView.getSettings().javaScriptEnabled = true
 
         editText.setOnEditorActionListener(OnEditorActionListener { v, actionId, event ->
             if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+
+                loading.visibility = View.GONE
+
                 val url = editText.text.toString()
                 webView.loadUrl(url)
 
@@ -36,6 +45,8 @@ class MainActivity : AppCompatActivity() {
             }
             false
         })
+
+
 
 
     }
